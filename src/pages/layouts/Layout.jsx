b11e0b2b94a2e0c2183e554e2e593e79d50a3dd5
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-03-28 13:16:27 +0100

    Fix: import statements

--- src/pages/layouts/Layout.jsx
+++ src/pages/layouts/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Logotype from '../../partials/sections/Logotype'
 import SideBar from '../../partials/sections/SideBar'
 import Header from '../../partials/sections/Header'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
