0ab298d5dc103bd0e2832790070aac65d1f2027d
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-03-28 13:08:10 +0100

    routesConfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
 import {useRoutes, Navigate} from 'react-router-dom'
+import Layout from './pages/layouts/Layout'
+import AuthLayout from './pages/layouts/AuthLayout'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Clients from './pages/protected/admin/Clients'
+import Members from './pages/protected/admin/Members'
+import Projects from './pages/protected/admin/Projects'
 
 const isAuthenticated = true
 const isAdmin = true
@@ -6,7 +13,25 @@ const isAdmin = true
 const ProtectedRoute = ({children}) => isAuthenticated ? children : <Navigate to = "/login" replace />
 const AdminRoute = ({children}) => isAuthenticated && isAdmin ? children : <Navigate to = "/projects" replace />
 
-const routesConfig = []
+const routesConfig = [
+  {
+    element: <AuthLayout/>,
+    children: [
+      { path: "/signin", element: <SignIn/> },
+      { path: "/signup", element: <SignUp/> }
+    ]
+  },
+  {
+    element: (<ProtectedRoute><Layout/></ProtectedRoute>),
+    children: [
+      { path: "/admin/projects", element: <Projects/> },
+      { path: "/admin/members", element: <AdminRoute><Members/></AdminRoute> },
+      { path: "/admin/clients", element: <AdminRoute><Clients/></AdminRoute> },
+      { path: "/", element: <Navigate to = "/admin/projects" replace /> }
+    ]
+  },
+  { path: "*", element: <Navigate to = "/signin" replace />  }
+]
 
 function App() {
   const routing = useRoutes(routesConfig)
