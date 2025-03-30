5a9cf6c5444a536b39fcb9f318680f77f5ba4d1c
Luiz Oliveira <luiz.oliveira@gronalund.com>
2025-03-30 13:50:37 +0200

    Fix: Providers

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { AuthProvider } from './contexts/AuthContext'
+import { ProjectsProvider } from './contexts/ProjectsContext'
+import { MembersProvider } from './contexts/MembersContext'
+import { ClientsProvider } from './contexts/ClientsContext'
 
 const Providers = ({children}) => {
   return (
     <>
-    <AuthProvider>
-        {children}
-    </AuthProvider>
+    <ProjectsProvider>
+      <AuthProvider>
+        <MembersProvider>
+          <ClientsProvider>
+            {children}
+          </ClientsProvider>
+        </MembersProvider>
+      </AuthProvider>
+    </ProjectsProvider>
     </>
   )
 }
